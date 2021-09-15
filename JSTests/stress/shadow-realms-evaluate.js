bb59e53647aae83f3db08e13ae90b2ffc04acb4e
Phillip Mates <pmates@igalia.com>
2021-09-15 15:18:41 +0200

    initial shadow realm evaluate stress test

---
+++ JSTests/stress/shadow-realms-evaluate.js
@@ -0,0 +1,63 @@
+//@ requireOptions("--useShadowRealm=1")
+
+function shouldBe(actual, expected) {
+    if (actual !== expected)
+        throw new Error(`expected ${expected} but got ${actual}`);
+}
+
+function shouldThrow(func, errorType, assertionFn) {
+    let error;
+    try {
+        func();
+    } catch (e) {
+        error = e;
+    }
+
+    if (!(error instanceof errorType))
+        throw new Error(`Expected ${errorType.name}!`);
+
+    assertionFn(error);
+}
+
+{
+    let realm = new ShadowRealm();
+    eval("1 + 1");
+    shouldBe(realm.evaluate("1"), 1);
+    realm.evaluate("var x = 1");
+    shouldBe(realm.evaluate("x"), 1);
+
+}
+
+{
+    let realm = new ShadowRealm();
+    globalThis.hi = 6;
+
+    shouldBe(realm.evaluate("globalThis.hi"), undefined);
+
+    realm.evaluate("globalThis.hi = 'fala amigo'");
+
+    shouldBe(realm.evaluate("globalThis.hi"), "fala amigo");
+    shouldBe(globalThis.hi, 6);
+}
+
+{
+    let realm = new ShadowRealm();
+    shouldThrow(
+      () => { realm.evaluate(".."); },
+      SyntaxError,
+      (err) => {
+          shouldBe($.globalObjectFor(err), globalThis);
+          shouldBe(String(err), `SyntaxError: Unexpected token '.'`);
+      });
+}
+
+{
+    let realm = new ShadowRealm();
+    shouldThrow(
+      () => { realm.evaluate("throw new Error('secret')"); },
+      TypeError,
+      (err) => {
+          shouldBe($.globalObjectFor(err), globalThis);
+          shouldBe(String(err), "TypeError: Error encountered during evaluation");
+      });
+}
--- Source/JavaScriptCore/jsc.cpp
+++ Source/JavaScriptCore/jsc.cpp
@@ -359,6 +359,7 @@ static JSC_DECLARE_HOST_FUNCTION(functionDollarCreateRealm);
 static JSC_DECLARE_HOST_FUNCTION(functionDollarEvalScript);
 static JSC_DECLARE_HOST_FUNCTION(functionDollarGC);
 static JSC_DECLARE_HOST_FUNCTION(functionDollarClearKeptObjects);
+static JSC_DECLARE_HOST_FUNCTION(functionDollarGlobalObjectFor);
 static JSC_DECLARE_HOST_FUNCTION(functionDollarAgentStart);
 static JSC_DECLARE_HOST_FUNCTION(functionDollarAgentReceiveBroadcast);
 static JSC_DECLARE_HOST_FUNCTION(functionDollarAgentReport);
@@ -638,6 +639,7 @@ private:
         addFunction(vm, dollar, "evalScript", functionDollarEvalScript, 1, static_cast<unsigned>(PropertyAttribute::None));
         addFunction(vm, dollar, "gc", functionDollarGC, 0, static_cast<unsigned>(PropertyAttribute::None));
         addFunction(vm, dollar, "clearKeptObjects", functionDollarClearKeptObjects, 0, static_cast<unsigned>(PropertyAttribute::None));
+        addFunction(vm, dollar, "globalObjectFor", functionDollarGlobalObjectFor, 1, static_cast<unsigned>(PropertyAttribute::None));
         
         dollar->putDirect(vm, Identifier::fromString(vm, "global"), globalThis());
         dollar->putDirectCustomAccessor(vm, Identifier::fromString(vm, "IsHTMLDDA"),
@@ -2045,6 +2047,21 @@ JSC_DEFINE_HOST_FUNCTION(functionDollarClearKeptObjects, (JSGlobalObject* global
     VM& vm = globalObject->vm();
     vm.finalizeSynchronousJSExecution();
     return JSValue::encode(jsUndefined());
+}
+
+JSC_DEFINE_HOST_FUNCTION(functionDollarGlobalObjectFor, (JSGlobalObject* globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    if (callFrame->argumentCount() < 1)
+        return JSValue::encode(throwException(globalObject, scope, createError(globalObject, "Not enough arguments"_s)));
+    JSValue arg = callFrame->argument(0);
+    if (arg.isCell()) {
+        Structure* structure = arg.structureOrNull(vm);
+        return JSValue::encode(structure->globalObject()->globalThis());
+    } else
+        return JSValue::encode(jsUndefined());
 }
 
 JSC_DEFINE_HOST_FUNCTION(functionDollarAgentStart, (JSGlobalObject* globalObject, CallFrame* callFrame))
